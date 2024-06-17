63f7f5cbf27fe7a066262d0c405a8f02e3a18fc6
chris <chrisfree1122@gmail.com>
2024-06-17 11:16:47 -0500

    elminando modulo por defecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from 'src/prisma.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
   imports: [AuthModule, UsuariosModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
